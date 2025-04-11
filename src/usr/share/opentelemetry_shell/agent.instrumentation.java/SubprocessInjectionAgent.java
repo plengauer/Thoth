e6651ed4022aa65ef9c9fc7daf3d9ce4899ee794
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-11 10:07:17 +0200

    Create SubprocessInjectionAgent.java

---
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -0,0 +1,7 @@
+import java.lang.instrument.Instrumentation;
+
+public class SubprocessInjectionAgent {
+    public static void premain(String args, Instrumentation instrumentation) {
+      
+    }
+}
