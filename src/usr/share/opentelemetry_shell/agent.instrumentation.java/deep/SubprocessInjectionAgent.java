f730d7a8a40822c8c1707667a2590994fb3711e2
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-11 10:54:55 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/deep/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/deep/SubprocessInjectionAgent.java
@@ -22,28 +22,5 @@ public class SubprocessInjectionAgent {
                e.printStackTrace();
            }
        });
-       instrumentation.addTransformer((loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
-           if (!"java/lang/Runtime".equals(className)) return null;
-           try {
-               CtClass ctClass = pool.get("java.lang.Runtime");
-
-               // Instrumenting the exec(String) method
-               CtMethod execMethod = ctClass.getDeclaredMethod("exec",
-                       new CtClass[]{pool.get("java.lang.String")});
-
-               execMethod.insertBefore(
-                       "{ $1 = \"sh -c '\" + $1 + \"'\"; }");
-                                      // Instrumenting exec(String[]) method
-               CtMethod execArrayMethod = ctClass.getDeclaredMethod("exec",
-                       new CtClass[]{pool.get("java.lang.String[]")});
-
-               execArrayMethod.insertBefore(
-                       "{ $1 = new String[] { \"sh\", \"-c\", String.join(\" \", $1) }; }");
-
-               return ctClass.toBytecode();
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
-       });
    }
 }
