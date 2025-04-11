88324fb0a03c7ae7e1c1ac076531bd2c4939e9c9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-11 13:43:52 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/deep/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/deep/SubprocessInjectionAgent.java
@@ -2,29 +2,30 @@ import javassist.*;
 import java.lang.instrument.Instrumentation;
 
 public class SubprocessInjectionAgent {
-
-   public static void premain(String args, Instrumentation instrumentation) throws Exception {
-       ClassPool pool = ClassPool.getDefault();
-       instrumentation.addTransformer((loader, className, classBeingRedefined, protectionDomain, classfileBuffer) -> {
-           if (!"java/lang/ProcessBuilder".equals(className)) return null;
-           try {
-               CtClass ctClass = pool.get("java.lang.ProcessBuilder");
-               CtMethod method = ctClass.getDeclaredMethod("start");
-               method.insertBefore(
-                  "{"
-                   + "java.util.List command = $0.command();"
-                   + "command.add(0, \"sh\");"
-                   + "command.add(1, \"-c\");"
-                   + "command.add(2, \".otel.sh\\n\" + command.remove(2));"
-                   + "command.add(3, \"java\");"
-                   + "}"
-                  // TODO modify env
-                  // optionally add otel_inject
-               );
-               return ctClass.toBytecode();
-           } catch (Exception e) {
-               e.printStackTrace();
-           }
-       });
-   }
+    public static void premain(String args, Instrumentation instrumentation) throws Exception {
+        instrumentation.addTransformer(new ClassFileTransformer() {
+            public byte[] transform(loader, className, classBeingRedefined, protectionDomain, classfileBuffer) {
+                if (!"java/lang/ProcessBuilder".equals(className)) return null;
+                try {
+                    ClassPool pool = ClassPool.getDefault();
+                    CtClass ctClass = pool.get("java.lang.ProcessBuilder");
+                    CtMethod method = ctClass.getDeclaredMethod("start");
+                    method.insertBefore(
+                      "{"
+                      + "java.util.List command = $0.command();"
+                      + "command.add(0, \"sh\");"
+                      + "command.add(1, \"-c\");"
+                      + "command.add(2, \".otel.sh\\n\" + command.remove(2));"
+                      + "command.add(3, \"java\");"
+                      + "}"
+                      // TODO modify env
+                      // optionally add otel_inject
+                    );
+                    return ctClass.toBytecode();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
