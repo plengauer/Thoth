90eb63f2f8fb199f4de8e376d230db39b4fe0ce4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-12 20:34:00 +0200

    Create agent.instrumentation.codeql.sh

---
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.codeql.sh
@@ -0,0 +1,6 @@
+#!/bin/false
+
+if \[ "${GITHUB_ACTIONS:-false}" = true ] &&  \[ -x "$0" ] && _otel_string_ends_with "$0" /codeql && \cat "$0" | \grep -qv _otel_inject; then
+  \sed -i 's~^"${CODEQL_JAVA_HOME}/bin/java"~_otel_inject "${CODEQL_JAVA_HOME}/bin/java"~g' "$0"
+  \eval '"exec"' "$(\xargs -0 sh -c '. otelapi.sh; _otel_escape_args "$@"' sh < /proc/$$/cmdline)"
+fi
