149cad22aaf4d5040644099376b511d023443762
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-04-15 12:47:19 +0200

    Update agent.instrumentation.github.actions.codeql.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.codeql.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.codeql.sh
@@ -1,7 +1,6 @@
 #!/bin/false
 
 if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ -x "$0" ] && _otel_string_ends_with "$0" /codeql && ! \cat "$0" | \grep -q _otel_inject; then
-  \sudo sed -i 's~'#!/bin/sh~#!/bin/bash\nset -x~g' "$0" # SKIP_DEPENDENCY_CHECK
   \sudo sed -i 's~"${CODEQL_JAVA_HOME}/bin/java"~_otel_inject "${CODEQL_JAVA_HOME}/bin/java"~g' "$0" # SKIP_DEPENDENCY_CHECK
   if \cat "$0" | \grep -q _otel_inject; then
     \eval '"exec"' "$(\xargs -0 sh -c '. otelapi.sh; _otel_escape_args "$@"' sh < /proc/$$/cmdline)"
