b05a3cfd5350f21bcbd2b9764def5fa33ce886d4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-05-10 18:02:40 +0200

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -56,10 +56,6 @@ gh_artifacts() {
 export -f gh_artifacts
 
 gh_artifact_download() {
-  local artifact_filename="$(mktemp)"
-  gh_curl /actions/runs/"$1"/artifacts'?per_page=1&'name="$3" | jq '.artifacts[0].id' | grep -v '^null$' | xargs -r -I '{}' bash -c 'gh_curl "$@"' bash /actions/artifacts/'{}'/zip --head | tr -d '\r' | grep '^location:' | cut -d ' ' -f 2- | xargs -r wget -O "$artifact_filename" && [ -r "$artifact_filename" ] && unzip "$artifact_filename" -d "$4" && rm "$artifact_filename"
-}
-gh_artifact_download() {
   node -e '
     const { DefaultArtifactClient } = require("@actions/artifact");
     const client = new DefaultArtifactClient()
@@ -73,6 +69,10 @@ gh_artifact_download() {
       .then(response => response.artifacts.find(artifact => artifact.name == "'"$3"'")?.id)
       .then(id => id ? client.downloadArtifact(id, { path: "'"$4"'", findBy }) : null);
   '
+}
+gh_artifact_download() {
+  local artifact_filename="$(mktemp)"
+  gh_curl /actions/runs/"$1"/artifacts'?per_page=1&'name="$3" | jq '.artifacts[0].id' | grep -v '^null$' | xargs -r -I '{}' bash -c 'gh_curl "$@"' bash /actions/artifacts/'{}'/zip --head | tr -d '\r' | grep '^location:' | cut -d ' ' -f 2- | xargs -r wget -O "$artifact_filename" && [ -r "$artifact_filename" ] && unzip "$artifact_filename" -d "$4" && rm "$artifact_filename"
 }
 export -f gh_artifact_download
 
