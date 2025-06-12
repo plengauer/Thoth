b7fb6763dbeefcd95a46a9473894871235658b95
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-06-12 09:10:31 +0200

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -1,6 +1,7 @@
 #!/bin/false
 
 gh_curl() {
+  echo curl -L --no-progress-meter --fail --retry 16 -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" "${GITHUB_API_URL:-https://api.github.com}"/repos/"$GITHUB_REPOSITORY""$@"
   curl -L --no-progress-meter --fail --retry 16 -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" "${GITHUB_API_URL:-https://api.github.com}"/repos/"$GITHUB_REPOSITORY""$@"
 }
 export -f gh_curl
