0df68a125b8ce465572c114d87ba2061f4c0bea9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-08 13:43:08 +0200

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -284,14 +284,8 @@ root4job_end() {
   fi
 
   while kill -0 "$observe_rate_limit_pid" 2> /dev/null; do sleep 1; done
-sleep 10 && set
-  timeout 60s sh -c 'while [ "$(pgrep -cf /opt/opentelemetry_shell/)" -gt 0 ]; do sleep 1; done' || true
-
-pgrep -cf /opt/opentelemetry_shell/ || true
-pgrep -f /opt/opentelemetry_shell/ || true
-fuser /tmp/*_opentelemetry_shell_*.pipe || true
-ps -ef || true
-
+  timeout 60s sh -xc 'while [ "$(pgrep -cf /opt/opentelemetry_shell/)" -gt 0 ]; do sleep 1; done' || true
+  
   if [ -n "${OTEL_SHELL_COLLECTOR_CONTAINER:-}" ]; then
     sudo docker stop "$OTEL_SHELL_COLLECTOR_CONTAINER"
     if [ -n "$INPUT_DEBUG" ]; then
