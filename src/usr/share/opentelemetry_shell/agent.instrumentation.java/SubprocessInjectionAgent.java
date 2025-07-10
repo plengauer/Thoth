2e8b04c1eb05de8582445d6287f20cf64861ceab
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-10 23:24:48 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -1,34 +1,29 @@
-import javassist.*;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.matcher.ElementMatchers;
 import java.lang.instrument.*;
 import java.io.*;
 
 public class SubprocessInjectionAgent {
     public static void premain(String args, Instrumentation instrumentation) throws Exception {
-        instrumentation.addTransformer(new ClassFileTransformer() {
-            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, java.security.ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-                if (!"java/lang/ProcessBuilder".equals(className)) return null;
-                try {
-                    ClassPool pool = ClassPool.getDefault();                    
-                    CtClass ctClass = pool.makeClass(new ByteArrayInputStream(classfileBuffer));
-                    CtMethod method = ctClass.getDeclaredMethod("start");
-                    method.insertBefore(
-                      "{"
-                      + "java.util.List command = $0.command();" // echo hello world
-                      + "command.add(0, \"sh\");" // sh echo hello world
-                      + "command.add(1, \"-c\");" // sh -c echo hello world
-                      + "command.add(2, \". otel.sh\\n\" + command.remove(2) + \" \\\"$@\\\"\");" // sh -c '. otel.sh \n echo "$@"' hello world
-                      + "command.add(3, \"java\");" // sh -c '. otel.sh \n echo "$@"' java hello world
-                      + "}"
-                      // TODO modify env
-                      // optionally add otel_inject
-                    );
-                    return ctClass.toBytecode();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return classfileBuffer;
-                }
-            }
-        }, true);
-        //instrumentation.retransformClasses(ProcessBuilder.class);
+        new AgentBuilder.Default()
+            .type(ElementMatchers.named("java.lang.ProcessImpl"))
+            .transform((builder, typeDescription, classLoader, module) ->
+                builder.method(ElementMatchers.named("start")
+                    .and(ElementMatchers.takesArguments(String[].class, Map.class, String.class, java.lang.ProcessBuilder.Redirect[].class, boolean.class)))
+                    .intercept(Advice.to(InjectCommandAdvice.class))
+            ).installOn(instrumentation);
+    }
+
+    public static class InjectCommandAdvice {
+        @Advice.OnMethodEnter
+        public static void onEnter(@Advice.Argument(value = 0, readOnly = false) String[] cmdarray) {
+            String[] oldcmdarray = cmdarray;
+            cmdarray = new String[3 + oldcmdarray.length];
+            cmdarray[0] = "/bin/sh";
+            cmdarray[1] = "-c";
+            cmdarray[2] = ". otel.sh\n_otel_inject \"" + oldcmdarray[0] + "\" \"$@\"";
+            System.arraycopy(oldcmdarray, 0, cmdarray, 3, cmdarray.length);
+        }
     }
 }
