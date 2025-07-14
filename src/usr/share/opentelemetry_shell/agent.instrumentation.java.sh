cd001c7f94da384752894d03eca1efc0d5e894e2
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-14 14:27:53 +0200

    Update agent.instrumentation.java.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
@@ -1,9 +1,8 @@
 #!/bin/false
 
 _otel_inject_java() {
-  # TODO injection into subprocesses
-  if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-FALSE}" = TRUE ] && \[ -r /opt/opentelemetry_shell/subprocessinjectionagent.jar ] && \[ -r /opt/opentelemetry_shell/rootcontextagent.jar ] && \[ -r /opt/opentelemetry_shell/opentelemetry-javaagent.jar ] && \[ "$(_otel_call "$1" --version | \head -n 1 | \cut -d ' ' -f 2 | \cut -d . -f 1)" -ge 8 ]; then
-    JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:-} -javaagent:/opt/opentelemetry_shell/opentelemetry-javaagent.jar -javaagent:/opt/opentelemetry_shell/rootcontextagent.jar -javaagent:/opt/opentelemetry_shell/subprocessinjectionagent.jar" _otel_call "$@"
+  if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-FALSE}" = TRUE ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar ] && \[ "$(_otel_call "$1" --version | \head -n 1 | \cut -d ' ' -f 2 | \cut -d . -f 1)" -ge "$(cat /usr/share/opentelemetry_shell/agent.instrumentation.java/version)" ]; then
+    JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:-} -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar" _otel_call "$@"
   else
     _otel_call "$@"
   fi
