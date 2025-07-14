7183f8284c573b5f27c9a0c2acbd4c48c778dc04
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-14 21:27:38 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -13,6 +13,7 @@ public class SubprocessInjectionAgent {
             .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
             .type(ElementMatchers.named("java.lang.ProcessImpl"))
             .transform((builder, typeDescription, classLoader, module, protectionDomain) ->
+                System.err.println("DEBUG DEBUG DEBUG found class");
                 builder.method(ElementMatchers.named("start").and(ElementMatchers.takesArguments(String[].class, Map.class, String.class, java.lang.ProcessBuilder.Redirect[].class, boolean.class)))
                     .intercept(Advice.to(InjectCommandAdvice.class))
             ).installOn(instrumentation);
