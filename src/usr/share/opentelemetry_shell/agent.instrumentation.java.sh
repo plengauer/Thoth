01b08651c3bfd3aad24f5a3f98ef1b33e1db2388
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-17 15:52:16 +0200

    Update agent.instrumentation.java.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
@@ -1,5 +1,7 @@
 #!/bin/false
 
+# TODO check if otel is already injected
+# TODO add to commandline instead of env var to suppress warning
 _otel_inject_java() {
   local version="$(_otel_call "$1" --version | \head -n 1 | \cut -d ' ' -f 2 | \cut -d . -f 1)"
   if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-FALSE}" = TRUE ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar ] && \[ "$version" -ge "$(cat /usr/share/opentelemetry_shell/agent.instrumentation.java/version)" ]; then
