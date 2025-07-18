8a8d5000f7e3b4c5cbbf2f3acf0206cd11c3a77f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-07-18 13:15:00 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -8,7 +8,6 @@ import net.bytebuddy.implementation.MethodDelegation;
 import java.lang.instrument.Instrumentation;
 import java.util.Map;
 import java.lang.reflect.Method;
-import java.lang.reflect.InvocationTargetException;
 
 public class SubprocessInjectionAgent {
     public static void premain(String args, Instrumentation instrumentation) throws Exception {
@@ -17,37 +16,11 @@ public class SubprocessInjectionAgent {
             .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
             .with(AgentBuilder.InitializationStrategy.NoOp.INSTANCE)
             .with(AgentBuilder.TypeStrategy.Default.REDEFINE)
-            // .with(AgentBuilder.Listener.StreamWriting.toSystemError())
-            .assureReadEdgeFromAndTo(instrumentation, InjectCommandInterceptor.class)
+            .assureReadEdgeFromAndTo(instrumentation, InjectCommandAdvice.class)
             .ignore(ElementMatchers.none())
             .type(ElementMatchers.named("java.lang.ProcessImpl"))
             .transform((builder, typeDescription, classLoader, module, protectionDomain) -> builder.visit(Advice.to(InjectCommandAdvice.class).on(ElementMatchers.named("start"))))
-            //.transform((builder, typeDescription, classLoader, module, protectionDomain) -> builder.method(ElementMatchers.named("start")).intercept(MethodDelegation.to(InjectCommandInterceptor.class)))
             .installOn(instrumentation);
-    }
-
-    public static class InjectCommandInterceptor {
-        @RuntimeType
-        public static Object intercept(@AllArguments Object[] args, @Origin Method method) throws Throwable {
-            System.err.println("DEBUG DEBUG DEBUG");
-            String[] oldcmdarray = (String[]) args[0];
-            String[] cmdarray = new String[3 + oldcmdarray.length];
-            cmdarray[0] = "/bin/sh";
-            cmdarray[1] = "-c";
-            cmdarray[2] = ". otel.sh\n_otel_inject \"" + oldcmdarray[0] + "\" \"$@\"";
-            System.arraycopy(oldcmdarray, 0, cmdarray, 3, oldcmdarray.length);
-            args[0] = cmdarray;
-            method.setAccessible(true);
-            try {
-                return method.invoke(null, args);
-            } catch (IllegalAccessException e) {
-                throw new Error("Here be dragons!");
-            } catch (IllegalArgumentException e) {
-                throw new Error("Here be dragons!");
-            } catch (InvocationTargetException e) {
-                throw e.getCause();
-            }
-        }
     }
 
     public static class InjectCommandAdvice {
@@ -59,7 +32,6 @@ public class SubprocessInjectionAgent {
             cmdarray[1] = "-c";
             cmdarray[2] = ". otel.sh\n_otel_inject \"" + oldcmdarray[0] + "\" \"$@\"";
             System.arraycopy(oldcmdarray, 0, cmdarray, 3, oldcmdarray.length);
-            System.err.println("DEBUG DEBUG DEBUG");
         }
     }
 }
