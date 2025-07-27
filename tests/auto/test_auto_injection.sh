9649c352a5c70f00387bdfb2dd74172af454d543
plengauer <p.lengauer@gmail.com>
2025-07-27 12:23:09 +0000

    refactored tests

--- tests/auto/test_auto_injection.sh
+++ tests/auto/test_auto_injection.sh
@@ -1,10 +1,19 @@
 . ./assert.sh
 . /usr/bin/opentelemetry_shell.sh
 
-sh auto/fail_no_auto.shell
+eval "$SHELL auto/fail_no_auto.sh"
 assert_equals 0 $?
-span="$(resolve_span '.resource.attributes."process.command_line" == "sh auto/fail_no_auto.shell"')"
+span="$(resolve_span '.resource.attributes."process.command_line" == "bash auto/fail_no_auto.sh"')"
 assert_equals "myspan" "$(\echo "$span" | jq -r '.name')"
-assert_equals "SpanKind.INTERNAL" "$(\echo "$span" | jq -r '.kind')"
-sh auto/fail.shell 42
+assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
+
+eval "$SHELL auto/fail.sh 42"
 assert_equals 42 $?
+
+if [ "$SHELL" = bash ]; then
+  bash auto/echo.sh
+  assert_equals 0 $?
+  span="$(resolve_span '.resource.attributes."process.command_line" == "bash auto/echo.sh"')"
+  assert_equals "echo hello world" "$(\echo "$span" | jq -r '.name')"
+  assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
+fi
---
+++ tests/auto/test_auto_injection_shebang.sh
@@ -0,0 +1,16 @@
+. ./assert.sh
+if [ "$(whoami)" = "root" ]; then
+  echo "#!/bin/sh" > /usr/bin/fail_no_auto.sh
+  cat auto/fail_no_auto.sh >> /usr/bin/fail_no_auto.sh
+  chmod +x /usr/bin/fail_no_auto.sh
+else
+  echo "#!/bin/sh" | sudo tee /usr/bin/fail_no_auto.sh
+  cat auto/fail_no_auto.sh | sudo tee -a /usr/bin/fail_no_auto.sh
+  sudo chmod +x /usr/bin/fail_no_auto.sh
+fi
+. /usr/bin/opentelemetry_shell.sh
+fail_no_auto.sh
+assert_equals 0 $?
+span="$(resolve_span '.resource.attributes."process.command_line" | contains("/fail_no_auto.sh")')"
+assert_equals "myspan" "$(\echo "$span" | jq -r '.name')"
+assert_equals "SpanKind.INTERNAL" $(\echo "$span" | jq -r '.kind')
