a1e5e47a433659e90b017e6dc773fd94133b6b0f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-04 21:13:05 +0200

    Update agent.instrumentation.java.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java.sh
@@ -3,7 +3,8 @@
 _otel_inject_java() {
   local version="$(_otel_call "$1" --version | \head -n 1 | \cut -d ' ' -f 2 | \cut -d . -f 1)"
   if \[ "${OTEL_SHELL_CONFIG_INJECT_DEEP:-FALSE}" = TRUE ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar ] && \[ -r /usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar ] && \[ "$version" -ge "$(cat /usr/share/opentelemetry_shell/agent.instrumentation.java/version)" ]; then
-    JAVA_TOOL_OPTIONS="${JAVA_TOOL_OPTIONS:-} -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar" _otel_call "$@"
+    local command="$1"; shift
+    _otel_call "$1" -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/opentelemetry-javaagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/rootcontextagent.jar -javaagent:/usr/share/opentelemetry_shell/agent.instrumentation.java/subprocessinjectionagent.jar "$@"
   else
     _otel_call "$@"
   fi
