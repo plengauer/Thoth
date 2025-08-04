5290bbae7daacbb6ee6366ca16e028eae6433e00
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-04 11:45:31 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -51,7 +51,7 @@ public class SubprocessInjectionAgent {
             if (environment != null) {
                 SpanContext spanContext = Span.current().getSpanContext();
                 environment.put("TRACEPARENT", String.format("%s-%s-%s-%s", "00", spanContext.getTraceId(), spanContext.getSpanId(), spanContext.getTraceFlags().asHex()));
-                environment.put("OTEL_SHELL_AUTO_INJECTED", "FALSE");
+                environment.put("OTEL_SHELL_AUTO_INJECTED", "TRUE");
             }
             String[] oldcmdarray = cmdarray;
             cmdarray = new String[3 + oldcmdarray.length];
