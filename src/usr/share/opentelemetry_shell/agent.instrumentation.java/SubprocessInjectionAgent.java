890f2307060e370b50ab17c84dd1feb6a0a2f2a9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-04 13:36:07 +0200

    Update SubprocessInjectionAgent.java

--- src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.java/SubprocessInjectionAgent.java
@@ -59,7 +59,6 @@ public class SubprocessInjectionAgent {
             cmdarray[1] = "-c";
             cmdarray[2] = ". otel.sh\n_otel_inject \"" + oldcmdarray[0] + "\" \"$@\"";
             System.arraycopy(oldcmdarray, 0, cmdarray, 3, oldcmdarray.length);
-            System.err.println(java.util.Arrays.toString(cmdarray));
         }
     }
 }
