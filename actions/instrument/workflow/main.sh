ebaf1ac02471d28d53fc805a2b2311880e3b8aa6
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-09 08:45:26 +0200

    Update main.sh

--- actions/instrument/workflow/main.sh
+++ actions/instrument/workflow/main.sh
@@ -65,8 +65,6 @@ fi
 
 times_dir="$(mktemp -d)"
 
-echo "::notice ::Observing $(jq < "$workflow_json" -r .html_url)"
-
 . otelapi.sh
 export OTEL_DISABLE_RESOURCE_DETECTION=TRUE
 _otel_resource_attributes_process() {
@@ -147,7 +145,7 @@ fi
 otel_span_activate "$workflow_span_handle"
 [ -z "${INPUT_DEBUG}" ] || echo "span workflow $TRACEPARENT $(jq < "$workflow_json" -r .name)" >&2
 if [ "$(jq < "$workflow_json" .conclusion -r)" = failure ]; then otel_span_error "$workflow_span_handle"; fi
-echo ::notice::"Trace ID: $(echo "$TRACEPARENT" | cut -d - -f 2), Span ID: $(echo "$TRACEPARENT" | cut -d - -f 3)"
+echo ::notice::"Trace ID: $(echo "$TRACEPARENT" | cut -d - -f 2), Span ID: $(echo "$TRACEPARENT" | cut -d - -f 3), Link: $(jq < "$workflow_json" -r .html_url)"
 otel_span_end "$workflow_span_handle" @"$workflow_ended_at"
 
 jq < "$jobs_json" -r --unbuffered '. | ["'"$TRACEPARENT"'", .id, .conclusion, .started_at, .completed_at, .name] | @tsv' | sed 's/\t/ /g' | while read -r TRACEPARENT job_id job_conclusion job_started_at job_completed_at job_name; do
