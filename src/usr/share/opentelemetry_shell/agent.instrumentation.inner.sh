b4283b26654e394e0f105a47f80e97a28037bf0f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-11 10:02:31 +0200

    Update agent.instrumentation.inner.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.inner.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.inner.sh
@@ -53,7 +53,7 @@ _otel_alias_prepend dumb-init _otel_inject_inner_command
 _otel_alias_prepend xargs _otel_inject_xargs
 
 _otel_inject_xargs() {
-  local args_length=$(($(\getconf ARG_MAX) - $(\env | \wc -c) - $(\echo "$*" | wc -c) - 4096)) # this is an estimation of the default
+  local args_length=$(($(\getconf ARG_MAX) - $(\env | \wc -c) - $(\echo "$*" | \wc -c) - 4096)) # this is an estimation of the default
   local args_length=$(($args_length - 4096)) # make sure we have enough room for proper injection
   OTEL_SHELL_INJECT_INNER_COMMAND_MORE_ARGS="-s $args_length" _otel_inject_inner_command "$@"
 }
