2227253359a807085675f765d561491e882b6feb
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-13 23:12:37 +0200

    Update id_printer.sh

--- actions/instrument/id_printer.sh
+++ actions/instrument/id_printer.sh
@@ -16,7 +16,5 @@ print_span_id() {
 
 print_dynatrace_link() {
   local endpoint="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}"
-  local trace_id="$(echo "$TRACEPARENT" | cut -d - -f 2)"
-  local span_id="$(echo "$TRACEPARENT" | cut -d - -f 3)"
-  echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-14d%3Bnow&'traceId="$trace_id"'&'spanId="$span_id";;
+  echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-14d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)"
 }
