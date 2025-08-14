22124544338d3f7e56fe4905ff5f2afae1befd57
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-14 12:22:44 +0200

    Update id_printer.sh

--- actions/instrument/shared/id_printer.sh
+++ actions/instrument/shared/id_printer.sh
@@ -1,22 +1,9 @@
 #!/bin/false
 
-print_trace_id() {
-  local trace_id="$(echo "$TRACEPARENT" | cut -d - -f 2)"
-  case "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}" in
-    *dynatrace*) echo "[$trade_id]($(print_dynatrace_link))";;
-    *) echo "$trace_id";;
-  esac
-}
-
-print_span_id() {
-  local span_id="$(echo "$TRACEPARENT" | cut -d - -f 3)"
-  case "${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}" in
-    *dynatrace*) echo "[$span_id]($(print_dynatrace_link))";;
-    *) echo "$span_id";;
-  esac
-}
-
 print_dynatrace_link() {
   local endpoint="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}"
-  echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-14d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)"
+  case "$endpoint" in
+    *dynatrace*) echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-14d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)";;
+    *) ;;
+  esac
 }
