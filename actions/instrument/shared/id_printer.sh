2ecea3e64a7f3c7c4a502ab4cd987bfff1fa206d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-14 17:17:33 +0200

    Fix id printing in logs

---
+++ actions/instrument/shared/id_printer.sh
@@ -0,0 +1,9 @@
+#!/bin/false
+
+print_trace_link() {
+  local endpoint="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}"
+  case "$endpoint" in
+    *dynatrace*) echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-14d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)";;
+    *) false;;
+  esac
+}
