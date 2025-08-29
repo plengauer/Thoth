c84d03568357c9fce4c4aeda10fb2f5f361f9fe7
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-08-29 08:40:51 +0200

    Update test_auto_background_job.sh

--- tests/auto/test_auto_background_job.sh
+++ tests/auto/test_auto_background_job.sh
@@ -1,7 +1,7 @@
-if ! type node; then exit 0; fi
+if ! type node || cat /etc/os-release | grep Alpine; then exit 0; fi
 . otel.sh
 nohup node -e "require('http').createServer(function (req, res) { console.log(req.method, req.url); res.writeHead(200); res.end(); }).listen(8080);" > /tmp/http.log &
 \sleep 5
-\ps -o 'pid,comm' | \grep node | \grep 'createServer'
-\ps -o 'pid,comm' | \grep node | \grep 'createServer' | \cut -d ' ' -f 1 | \xargs -I '{}' kill -9 '{}'
+\ps -o 'pid,command' | \grep node | \grep 'createServer'
+\ps -o 'pid,command' | \grep node | \grep 'createServer' | \cut -d ' ' -f 1 | \xargs -I '{}' kill -9 '{}'
 
