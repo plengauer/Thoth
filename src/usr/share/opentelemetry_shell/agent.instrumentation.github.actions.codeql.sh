99c68ef12ff1262a8e862b1ce99af35e0067445a
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-09-10 20:01:27 +0200

    Fix CodeQL injection

--- src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.codeql.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.codeql.sh
@@ -3,8 +3,8 @@
 if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ -x "$0" ] && _otel_string_ends_with "$0" /codeql && ! \cat "$0" | \grep -q _otel_inject; then
   \sudo sed -i 's~#!/bin/sh~#!/bin/sh\n. otel.sh~g' "$0" # SKIP_DEPENDENCY_CHECK
   \sudo sed -i 's~#!/bin/bash~#!/bin/bash\n. otel.sh~g' "$0" # SKIP_DEPENDENCY_CHECK
-  \sudo sed -i 's~"$CODEQL_DIST/codeql"~_otel_inject "$CODEQL_DIST/codeql"~g' "$0" # SKIP_DEPENDENCY_CHECK
-  \sudo sed -i 's~"${CODEQL_DIST}/codeql"~_otel_inject "${CODEQL_DIST}/codeql"~g' "$0" # SKIP_DEPENDENCY_CHECK
+  # \sudo sed -i 's~"$CODEQL_DIST/codeql"~_otel_inject "$CODEQL_DIST/codeql"~g' "$0" # SKIP_DEPENDENCY_CHECK
+  # \sudo sed -i 's~"${CODEQL_DIST}/codeql"~_otel_inject "${CODEQL_DIST}/codeql"~g' "$0" # SKIP_DEPENDENCY_CHECK
   \sudo sed -i 's~"$CODEQL_JAVA_HOME/bin/java"~_otel_inject "$CODEQL_JAVA_HOME/bin/java"~g' "$0" # SKIP_DEPENDENCY_CHECK
   \sudo sed -i 's~"${CODEQL_JAVA_HOME}/bin/java"~_otel_inject "${CODEQL_JAVA_HOME}/bin/java"~g' "$0" # SKIP_DEPENDENCY_CHECK
   \sudo sed -i 's~\. "$chainfile"~\\. "$chainfile"~g' "$0" # SKIP_DEPENDENCY_CHECK
