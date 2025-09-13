2a5e67dd8ef83f4f65087ce0b65d0216ce6c3baa
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-09-13 11:03:21 +0200

    Update decorate_action.sh

--- actions/instrument/job/decorate_action.sh
+++ actions/instrument/job/decorate_action.sh
@@ -44,13 +44,6 @@ _otel_resource_attributes_custom() {
 }
 eval "$(cat "$_OTEL_GITHUB_STEP_AGENT_INSTRUMENTATION_FILE" | grep -v '_otel_alias_prepend ')"
 
-echo "DEBUG DEBUG DEBUG $*"
-ls -la "$1"
-ls -la /home/runner/work/Thoth/Thoth/actions/instrument/job/relocated_bin/node20/node
-ls -la /home/runner/work/Thoth/Thoth/actions/instrument/job/relocated_bin/node24/node
-find /home/runner/work/Thoth/Thoth/actions/instrument/job/relocated_bin
-
-
 otel_init
 time_start="$(date +%s.%N)"
 span_handle="$(otel_span_start INTERNAL "${GITHUB_STEP:-$GITHUB_ACTION}")"
