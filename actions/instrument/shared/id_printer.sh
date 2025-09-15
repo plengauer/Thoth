78ea1a398478f4bc67d6da5dc62157f2ab311d08
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-09-15 22:03:38 +0200

    Update id_printer.sh

--- actions/instrument/shared/id_printer.sh
+++ actions/instrument/shared/id_printer.sh
@@ -3,7 +3,7 @@
 print_trace_link() {
   local endpoint="${OTEL_EXPORTER_OTLP_TRACES_ENDPOINT:-${OTEL_EXPORTER_OTLP_ENDPOINT:-http://localhost:4318}}"
   case "$endpoint" in
-    *dynatrace*) echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-7d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)";;
+    *dynatrace*) echo "$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f 3- | rev)".apps."$(echo "$endpoint" | cut -d / -f -3 | rev | cut -d . -f -2 | rev)"/ui/apps/dynatrace.distributedtracing/explorer'?sidebar=u%2Cfalse&tf=now-7d%3Bnow&'traceId="$(echo "$TRACEPARENT" | cut -d - -f 2)"'&'spanId="$(echo "$TRACEPARENT" | cut -d - -f 3)"'&'"tt=$(date +%Y-%M-%dT%H:%M:%S.%N%:z | jq -sRr @uri)";;
     *) false;;
   esac
 }
