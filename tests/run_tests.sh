e3d5c8481392a62f5733b4965987cca9fb149f61
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-10-02 14:59:18 +0200

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -15,12 +15,12 @@ for dir in unit sdk auto integration; do
   { find $dir -iname 'test_*.sh'; find $dir -iname 'test_*.'"$SHELL"; } | sort -u | while read -r file; do
     rm /tmp/opentelemetry_shell_*_instrumentation_cache_*.aliases 2> /dev/null || true
     export OTEL_EXPORT_LOCATION="$(mktemp -u)".sdk.out
-    export OTEL_SHELL_SDK_OUTPUT_REDIRECT="$(mktemp -u -p "$(mktemp -d)")".pipe
+    export OTEL_SHELL_SDK_STDOUT_REDIRECT="$(mktemp -u -p "$(mktemp -d)")".pipe
     export OTEL_TRACES_EXPORTER=console
     export OTEL_METRICS_EXPORTER=console
     export OTEL_LOGS_EXPORTER=console
-    mkfifo -m 666 "$OTEL_SHELL_SDK_OUTPUT_REDIRECT"
-    ( while true; do cat "$OTEL_SHELL_SDK_OUTPUT_REDIRECT" >> "$OTEL_EXPORT_LOCATION"; done & )
+    mkfifo -m 666 "$OTEL_SHELL_SDK_STDOUT_REDIRECT"
+    ( while true; do cat "$OTEL_SHELL_SDK_STDOUT_REDIRECT" >> "$OTEL_EXPORT_LOCATION"; done & )
     echo "running $file"
     options='-f -u'
     if [ "$TEST_SHELL" = bash ]; then
