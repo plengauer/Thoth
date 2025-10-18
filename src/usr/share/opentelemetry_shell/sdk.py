c4c3774f3768bc8fa0929c798846984112316f66
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-10-18 11:31:45 +0200

    Remove SDK command print statement
    Remove debug print statement for SDK command.

--- src/usr/share/opentelemetry_shell/sdk.py
+++ src/usr/share/opentelemetry_shell/sdk.py
@@ -30,7 +30,6 @@ def main():
         line = line.strip()
         if len(line) == 0:
             continue
-        print('SDK Command: ' + line, file=sys.stderr)
         tokens = line.split(' ', 1)
         tokens = [token for token in tokens if token]
         try:
