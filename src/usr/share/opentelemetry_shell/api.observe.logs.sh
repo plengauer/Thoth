20a4ece5a60ceb6fbb46483ddea2d8e825f11fd9
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-09 21:11:03 +0100

    Update api.observe.logs.sh

--- src/usr/share/opentelemetry_shell/api.observe.logs.sh
+++ src/usr/share/opentelemetry_shell/api.observe.logs.sh
@@ -10,6 +10,9 @@ _otel_call_and_record_logs() {
   while IFS= \read -r line; do _otel_log_record "$traceparent" auto 0 "$line"; \printf '%s\n' "$line" >&2; done < "$stderr_logs" 1> /dev/null &
   local exit_code=0
   $call_command "$@" 2> "$stderr_logs" || local exit_code="$?"
+  # we accept, by not waiting for the background job above, that there is slight chance of the last few lines of stderr may be interleaved with the next few lines of the parent.
+  # alternatively, we would have to wait, but we do not know if the stderr stream is not opened by a background job of the child and will never close
+  # the control flow of this must continue when the command terminates, which is not the same as when the stream closes (the stream may be inherited to others)
   \rm "$stderr_logs" 2> /dev/null
   return "$exit_code"
 }
