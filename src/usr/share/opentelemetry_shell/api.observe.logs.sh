8811975e488a062007bee5a9ccdd86e0cb434be1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-10 14:00:19 +0100

    Fix background job handling by not treating a commend terminating the same as its streams closing

--- src/usr/share/opentelemetry_shell/api.observe.logs.sh
+++ src/usr/share/opentelemetry_shell/api.observe.logs.sh
@@ -1,21 +1,17 @@
 #!/bin/false
 
 _otel_call_and_record_logs() {
-  case "$-" in
-    *m*) local job_control=1; \set +m;;
-    *) local job_control=0;;
-  esac
   local call_command="$1"; shift
   local traceparent="$TRACEPARENT"
   local stderr_logs="$(\mktemp -u -p "$_otel_shell_pipe_dir")_opentelemetry_shell_$$.stderr.logs.pipe"
   \mkfifo ${_otel_mkfifo_flags:-} "$stderr_logs"
-  while IFS=$'\n' \read -r line; do _otel_log_record "$traceparent" auto 0 "$line"; \printf '%s\n' "$line" >&2; done < "$stderr_logs" &
-  local stderr_pid="$!"
+  while IFS=$'\n' \read -r line; do _otel_log_record "$traceparent" auto 0 "$line"; \printf '%s\n' "$line" >&2; done < "$stderr_logs" 1> /dev/null &
   local exit_code=0
   $call_command "$@" 2> "$stderr_logs" || local exit_code="$?"
-  \wait "$stderr_pid"
+  # we accept, by not waiting for the background job above, that there is slight chance of the last few lines of stderr may be interleaved with the next few lines of the parent.
+  # alternatively, we would have to wait, but we do not know if the stderr stream is not opened by a background job of the child and will never close
+  # the control flow of this must continue when the command terminates, which is not the same as when the stream closes (the stream may be inherited to others)
   \rm "$stderr_logs" 2> /dev/null
-  if \[ "$job_control" = 1 ]; then \set -m; fi
   return "$exit_code"
 }
 
