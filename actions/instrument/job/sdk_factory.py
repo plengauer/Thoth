8d158d754be1077e85a297d0d35ff84919f345a4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-13 09:24:41 +0100

    Update sdk_factory.py

--- actions/instrument/job/sdk_factory.py
+++ actions/instrument/job/sdk_factory.py
@@ -1,14 +1,20 @@
 import sys
 import os
-from /usr/share/opentelemetry_shell/sdk.py import run
+
+import importlib.util
+spec = importlib.util.spec_from_file_location("sdk", "/usr/share/opentelemetry_shell/sdk.py")
+helpers = importlib.util.module_from_spec(spec)
+sys.modules["sdk"] = helpers
+spec.loader.exec_module(helpers)
 
 for line in sys.stdin:
-  tokens = line.split(2, ' ');
+  line = line.strip()
+  tokens = line.split(" ", 2)
   scope = tokens[0]
   version = tokens[1]
   pipe = tokens[2]
-  pid = os.fork();
+  pid = os.fork()
   if pid != 0:
     continue
   with open(pipe) as commands:
-    run(scope, version, commands)
+    sdk.run(scope, version, commands)
