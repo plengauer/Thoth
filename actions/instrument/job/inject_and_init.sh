236f212ea5c47a12a012df80a41c460b76f26ef1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-13 15:28:03 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -440,7 +440,7 @@ echo "::group::Start Observation"
 traceparent_file="$(mktemp -u)"
 export OTEL_SHELL_SDK_FACTORY_PIPE=/tmp/otel_shell/sdk_factory.pipe
 mkdir -p "${OTEL_SHELL_SDK_FACTORY_PIPE%/*}"
-mkfifo -m 666 /tmp/opentelemetry_shell.github.debug.log "$OTEL_SHELL_SDK_FACTORY_PIPE" # subdirectory to avoid sticky bit
+mkfifo /tmp/opentelemetry_shell.github.debug.log "$OTEL_SHELL_SDK_FACTORY_PIPE" # subdirectory to avoid sticky bit
 wait # make sure we wait for all background jobs before we actually start
 nohup /opt/opentelemetry_shell/venv/bin/python sdk_factory.py "$OTEL_SHELL_SDK_FACTORY_PIPE" &> /dev/null &
 nohup bash -c 'root4job "$@"' bash "$traceparent_file" &> /dev/null &
