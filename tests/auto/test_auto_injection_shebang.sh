ea14b745d5d56fbab4e52d1d500be99c7df291bc
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-13 13:15:33 +0100

    Update test_auto_injection_shebang.sh

--- tests/auto/test_auto_injection_shebang.sh
+++ tests/auto/test_auto_injection_shebang.sh
@@ -1,10 +1,10 @@
 . ./assert.sh
 if [ "$(whoami)" = "root" ]; then
-  echo "#!/bin/sh -x" > /usr/bin/fail_no_auto.sh
+  echo "#!/bin/sh" > /usr/bin/fail_no_auto.sh
   cat auto/fail_no_auto.sh >> /usr/bin/fail_no_auto.sh
   chmod +x /usr/bin/fail_no_auto.sh
 else
-  echo "#!/bin/sh -x" | sudo tee /usr/bin/fail_no_auto.sh
+  echo "#!/bin/sh" | sudo tee /usr/bin/fail_no_auto.sh
   cat auto/fail_no_auto.sh | sudo tee -a /usr/bin/fail_no_auto.sh
   sudo chmod +x /usr/bin/fail_no_auto.sh
 fi
