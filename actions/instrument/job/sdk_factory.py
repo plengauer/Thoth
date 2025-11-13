26263c60fa6ad43bf2d9c524e58bc21b0c951b86
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-13 13:07:07 +0100

    Update sdk_factory.py

--- actions/instrument/job/sdk_factory.py
+++ actions/instrument/job/sdk_factory.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from threading import Thread
+import signal
 
 import importlib.util
 spec = importlib.util.spec_from_file_location("sdk", "/usr/share/opentelemetry_shell/sdk.py")
@@ -8,18 +8,24 @@ sdk = importlib.util.module_from_spec(spec)
 sys.modules["sdk"] = sdk
 spec.loader.exec_module(sdk)
 
-for line in sys.stdin:
-  line = line.strip()
-  if line == "EOF":
-    sys.exit()
-  tokens = line.split(" ", 2)
-  scope = tokens[0]
-  version = tokens[1]
-  pipe = tokens[2]
-  pid = os.fork()
-  if pid != 0:
-    Thread().start(target=os.waitpid, args=(pid, 0, ))
-  else
-    sys.stdin.close()
-    with open(pipe) as commands:
-      sdk.run(scope, version, commands)
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+while True:
+  with open(sys.argv[1]) as pipe:
+    try:
+      for line in pipe:
+        line = line.strip()
+        if line == "EOF":
+          sys.exit()
+        tokens = line.split(" ", 2)
+        scope = tokens[0]
+        version = tokens[1]
+        pipe = tokens[2]
+        pid = os.fork()
+        if pid != 0:
+          continue
+        sys.stdin.close()
+        with open(pipe) as commands:
+          sdk.run(scope, version, commands)
+    except:
+      pass
