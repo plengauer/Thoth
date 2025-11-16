0a1990e0becb1237e29d0d959445e55b18dfa428
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-16 10:54:39 +0100

    Refactor error handling in install.sh script

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -20,12 +20,10 @@ if ! type otel.sh 2> /dev/null; then
     sudo -E -H eatmydata apt-get -o Binary::apt::APT::Keep-Downloaded-Packages=true install -y "$debian_file"
   else
     false
-  fi \
-    || (
-      echo ::warning::'Cannot find release for specified tag, falling back to latest. This may be due to tags that haven't finished building yet (like in a fresh fork), in which case this will resolve automatically.' >&2
+  fi || (
+      echo ::warning::'Cannot find release for specified tag, falling back to latest. This may be due to tags that haven'\''t finished building yet (like in a fresh fork), in which case this will resolve automatically.' >&2
       curl --fail -L -s -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" "${GITHUB_API_URL:-https://api.github.com}"/repos/"$GITHUB_ACTION_REPOSITORY"/releases/latest | jq '.assets[] | select(.name | endswith(".deb")) | .url' -r | xargs -0 -I '{}' wget --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" --header 'Accept: application/octet-stream' -O /tmp/package.deb '{}'
       sudo -E -H eatmydata apt-get install -y /tmp/package.deb
       rm /tmp/package.deb
-    ) \
-    || ( echo ::warning::'Cannot find any release in repository, falling back to root repository' >&2 && wget -O - https://raw.githubusercontent.com/plengauer/Thoth/main/INSTALL.sh | sh -e )
+    ) || ( echo ::warning::'Cannot find any release in repository, falling back to root repository' >&2 && wget -O - https://raw.githubusercontent.com/plengauer/Thoth/main/INSTALL.sh | sh -e )
 fi
