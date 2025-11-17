b3fecd0d9d7e48440533e1d48828cbe2780f1cd2
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-17 14:27:20 +0100

    Update agent.sh

--- src/usr/share/opentelemetry_shell/agent.sh
+++ src/usr/share/opentelemetry_shell/agent.sh
@@ -448,6 +448,7 @@ command() {
 }
 
 _otel_inject() {
+  \set -x
   if _otel_string_contains "$1" / && \[ -x "$1" ]; then
     local path="$1"
     if ! \alias "${path##*/}" 1> /dev/null 2> /dev/null; then # in case its an absolute command that is not on the path at all, we need to make sure it is to have proper shebang resolution and the resulting instrumentation on hand
@@ -464,6 +465,7 @@ _otel_inject() {
     \eval "set -- $instrumentation $(_otel_escape_args "$@")"
   fi
   \eval "$(_otel_escape_args "$@")"
+  \set +x
 }
 
 _otel_start_script() {
