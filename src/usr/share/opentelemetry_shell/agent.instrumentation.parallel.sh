2e926fc902bab90435a99df8f5bb7a4c01db85e3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-18 01:03:34 +0100

    Refactor _otel_call function for simplicity (#2458)

--- src/usr/share/opentelemetry_shell/agent.instrumentation.parallel.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.parallel.sh
@@ -93,7 +93,7 @@ _otel_inject_parallel_arguments() {
 _otel_inject_parallel() {
   local cmdline="$(_otel_dollar_star "$@")"
   local cmdline="${cmdline#\\}"
-  OTEL_SHELL_COMMANDLINE_OVERRIDE="$cmdline" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE="0" OTEL_SHELL_AUTO_INJECTED=TRUE \eval _otel_call "$(_otel_inject_parallel_arguments "$@")"
+  \eval OTEL_SHELL_COMMANDLINE_OVERRIDE="$(_otel_escape_arg "$cmdline")" OTEL_SHELL_COMMANDLINE_OVERRIDE_SIGNATURE=0 OTEL_SHELL_AUTO_INJECTED=TRUE _otel_call "$(_otel_inject_parallel_arguments "$@")"
 }
 
 _otel_alias_prepend parallel _otel_inject_parallel
