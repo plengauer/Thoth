c3ee678d87d2512f8155b8b1308f6aa5d6663734
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-11-23 19:28:47 +0100

    Add job matrix attributes to job span (#2503)

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -415,6 +415,7 @@ root4job() {
   fi
   otel_span_attribute_typed $span_handle int github.actions.job.id="${GITHUB_JOB_ID:-}"
   otel_span_attribute_typed $span_handle string github.actions.job.name="${OTEL_SHELL_GITHUB_JOB:-$GITHUB_JOB}"
+  printf '%s' "$INPUT___JOB_MATRIX" | jq 'to_entries | .[] | [ .key, .value ] | @tsv' -r | sed 's/\t/ /g' | while read -r key value; do otel_span_attribute_typed $span_handle string github.actions.job.matrix."$key"="$value"; done
   otel_span_attribute_typed $span_handle string github.actions.runner.name="$RUNNER_NAME"
   otel_span_attribute_typed $span_handle string github.actions.runner.os="$RUNNER_OS"
   otel_span_attribute_typed $span_handle string github.actions.runner.arch="$RUNNER_ARCH"
