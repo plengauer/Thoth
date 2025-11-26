e26c0a1ef3b53aecbd996f7dd24efdab24b56e2d
plengauer <100447901+plengauer@users.noreply.github.com>
2025-11-26 00:43:49 +0000

    Set non-interactive frontend to improve flaky tests (#2527) Co-authored-by: Philipp Lengauer <100447901+plengauer@users.noreply.github.com>

--- tests/integration/test_integration_debconf_second.sh
+++ tests/integration/test_integration_debconf_second.sh
@@ -2,6 +2,7 @@
 if ! [ -f /usr/share/debconf/confmodule ]; then exit 0; fi
 . /usr/bin/opentelemetry_shell.sh
 set +u
+export DEBIAN_FRONTEND=noninteractive
 . /usr/share/debconf/confmodule
 db_get $(debconf-show $(debconf-show --listowners | head -n1) | sed 's/^\* //' | sed 's/  //' | cut -d: -f1 | head -n1)
 assert_equals 0 "$?"
