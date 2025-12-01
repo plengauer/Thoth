c4bae93501eea9993f7d5d7e374fabc2428b1b9f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-01 23:38:52 +0100

    Update inject_and_init.js

--- actions/instrument/job/inject_and_init.js
+++ actions/instrument/job/inject_and_init.js
@@ -2,6 +2,7 @@ const process = require('process')
 const { spawn } = require('child_process');
 
 function run(executable, args = []) {
+  console.log(process.platform);
   if (process.platform != 'linux') return;
   return new Promise((resolve, reject) => {
     const process = spawn(executable, args, { cwd: __dirname, stdio: 'inherit' });
