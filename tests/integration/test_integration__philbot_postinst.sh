acfe4fbe1b397f9511c9e8506638afd292a35596
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-06 22:08:07 +0100

    Update test_integration__philbot_postinst.sh

--- tests/integration/test_integration__philbot_postinst.sh
+++ tests/integration/test_integration__philbot_postinst.sh
@@ -1,12 +1,13 @@
 if ! \[ -f /usr/share/debconf/confmodule ]; then exit 0; fi
 \export DEBIAN_FRONTEND=noninteractive
 
-if \[ -r populate.sh ]; then
+if \[ -x populate.sh ]; then
   . otel.sh
 else
   \echo '. /usr/share/debconf/confmodule
   echo hello world
   ' > populate.sh
+  chmod +x populate.sh
 fi
 
 sh -e populate.sh
