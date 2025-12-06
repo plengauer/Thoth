4e2319d5821f4e92423527366fa9b7e773211b8b
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-06 21:45:59 +0100

    Fix syntax issues in test_integration__philbot_postinst.sh

--- tests/integration/test_integration__philbot_postinst.sh
+++ tests/integration/test_integration__philbot_postinst.sh
@@ -1,11 +1,10 @@
-. ./assert.sh
-if ! [ -f /usr/share/debconf/confmodule ]; then exit 0; fi
-export DEBIAN_FRONTEND=noninteractive
+if ! \[ -f /usr/share/debconf/confmodule ]; then exit 0; fi
+\export DEBIAN_FRONTEND=noninteractive
 
-if [ -r populate.sh ]; then
+if \[ -r populate.sh ]; then
   . otel.sh
 else
-  echo '. /usr/share/debconf/confmodule
+  \echo '. /usr/share/debconf/confmodule
   echo hello world
   ' > populate.sh
 fi
