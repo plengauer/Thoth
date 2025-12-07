023d650f76ba60fb9f2235b41c864a63e6e68a45
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-07 12:07:51 +0100

    Update test_integration__thoth_list_node_images.sh

--- tests/integration/test_integration__thoth_list_node_images.sh
+++ tests/integration/test_integration__thoth_list_node_images.sh
@@ -5,7 +5,7 @@ export OTEL_SHELL_CONFIG_INJECT_DEEP=TRUE
 export OTEL_SHELL_CONFIG_OBSERVE_SUBPROCESSES=TRUE
 . otel.sh
 ( cd ../src/usr/share/opentelemetry_shell/agent.instrumentation.node && npm install )
-seq 0 9 | while read -r line; do
+seq 0 999 | while read -r line; do
   version_min="$(cat ../src/usr/share/opentelemetry_shell/agent.instrumentation.node/node_modules/@opentelemetry/*/package.json | jq -r .engines.node | grep -v null | cut -d ' ' -f 1 | tr -d ^ | cut -d = -f 2 | cut -d . -f 1 | sort -n -u | tail -n 1)"
   version_max="$(cat ../src/usr/share/opentelemetry_shell/agent.instrumentation.node/Dockerfile | grep '^FROM ' | cut -d ' ' -f 2 | cut -d : -f 2)"
   seq "$version_min" "$version_max" | jq -s -c | xargs -0 -I {} echo 'versions={}'
