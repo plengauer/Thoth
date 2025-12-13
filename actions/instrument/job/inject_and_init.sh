6547aa8848836dd60a0cafbb9d75ee8b1b07f617
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-13 15:43:38 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -208,7 +208,7 @@ echo "$new_binary_dir" >> "$GITHUB_PATH"
 ( if type dash; then gcc -o "$new_binary_dir"/dash forward.c -DEXECUTABLE="$(which dash)" -DARG1="$GITHUB_ACTION_PATH"/decorate_action_run.sh -DARG2="$(which dash)"; fi 2>&1 | perl -0777 -pe '' ) &
 ( if type bash; then gcc -o "$new_binary_dir"/bash forward.c -DEXECUTABLE="$(which bash)" -DARG1="$GITHUB_ACTION_PATH"/decorate_action_run.sh -DARG2="$(which bash)"; fi 2>&1 | perl -0777 -pe '' ) &
 for node_path in "$(readlink -f /proc/*/exe | grep '/Runner.Worker$' | rev | cut -d / -f 4- | rev)"/*/externals/node*/bin/node; do
-  dir_path_new="$relocated_binary_dir"/"$(echo "$node_path" | rev | cut -d / -f 3 | rev)"
+  dir_path_new="$relocated_binary_dir"/"$(echo "$node_path" | rev | cut -d / -f 3 | rev)"-"$(echo "$node_path" | md5sum | cut -d ' ' -f 1)"
   mkdir "$dir_path_new"
   node_path_new="$dir_path_new"/node
   mv "$node_path" "$node_path_new"
