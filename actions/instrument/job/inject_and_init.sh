07396f0a0b1bc7a937fcd3e8f0de590726e6c281
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-14 22:34:17 +0100

    Update inject_and_init.sh

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -2,7 +2,7 @@
 set -e -o pipefail
 if [ -n "$INPUT_DEBUG" ]; then set -mx; fi
 FAST_DEB_INSTALL="${FAST_DEB_INSTALL:-TRUE}"
-ASYNC_INIT="${ASYNC_INIT:-FALSE}" # TRUE
+ASYNC_INIT="${ASYNC_INIT:-TRUE}"
 [ -z "$INPUT_DEBUG" ] || ASYNC_INIT=FALSE
 set -x
 
