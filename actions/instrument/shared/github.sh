08a012fd2cb04d8ea30d093c4df3d24ac12b00b4
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2025-12-21 11:14:06 +0100

    Update github.sh

--- actions/instrument/shared/github.sh
+++ actions/instrument/shared/github.sh
@@ -20,6 +20,16 @@ gh_rate_limit() {
 }
 export -f gh_rate_limit
 
+gh_ensure_min_rate_limit_remaining() {
+  local threshold="$1"
+  local delay=1
+  while [ "$(gh_rate_limit | jq '.resources.core | .remaining / .limit * 100 | floor' -r)" -lt "$(echo "$threshold" | jq '. * 100 | floor')" ]; do
+    sleep "$delay"
+    local delay=$((delay * 2))
+  done
+}
+export -f gh_ensure_rate_limit
+
 gh_releases() {
   GITHUB_REPOSITORY="$GITHUB_ACTION_REPOSITORY" gh_curl_paginated /releases'?per_page=100'
 }
