66bac759fb964adc2c11b30a73bee68a6357e7a5
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-01-01 14:13:59 +0100

    Ensure sequential execution of test scripts
    Added a wait command to ensure sequential execution of tests.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -38,6 +38,7 @@ for dir in unit sdk auto integration performance; do
     chmod 0666 "$stdout" "$stderr"
     export OTEL_SHELL_SDK_STDERR_REDIRECT="$stderr"
     ( timeout $((60 * 60 * 3)) $TEST_SHELL $options "$file" 1> "$stdout" 2> "$stderr" && echo "$file SUCCEEDED" || (echo "$file FAILED" && echo "stdout:" && cat "$stdout" && echo "stderr:" && cat "$stderr" && echo "otlp:" && cat "$OTEL_EXPORT_LOCATION" && touch "$failed_flag" && exit 1) ) 2>&1 | perl -e '$| = 1; print while <>;' &
+    wait # TODO force sequential for now
     [ "$dir" != performance ] || wait
   done < <({ find $dir -iname 'test_*.sh'; find $dir -iname 'test_*.'"$SHELL"; } | sort -u)
   wait
