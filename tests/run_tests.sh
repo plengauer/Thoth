8ded83f9de783273a8881a4adc361ceb63ffa51d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-02-14 21:05:26 +0100

    Update run_tests.sh

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -37,7 +37,7 @@ for dir in unit sdk auto integration performance; do
     touch "$stdout" "$stderr"
     chmod 0666 "$stdout" "$stderr"
     export OTEL_SHELL_SDK_STDERR_REDIRECT="$stderr"
-    ( timeout $((60 * 60 * 3)) $TEST_SHELL $options "$file" 1> "$stdout" 2> "$stderr" && echo "$file SUCCEEDED" || (echo "$file FAILED" && echo "stdout:" && cat "$stdout" && echo "stderr:" && cat "$stderr" && echo "otlp:" && cat "$OTEL_EXPORT_LOCATION" && touch "$failed_flag" && exit 1) ) 2>&1 | perl -e '$| = 1; print while <>;' &
+    ( timeout $((60 * 3)) $TEST_SHELL $options "$file" 1> "$stdout" 2> "$stderr" && echo "$file SUCCEEDED" || (echo "$file FAILED" && echo "stdout:" && cat "$stdout" && echo "stderr:" && cat "$stderr" && echo "otlp:" && cat "$OTEL_EXPORT_LOCATION" && touch "$failed_flag" && exit 1) ) 2>&1 | perl -e '$| = 1; print while <>;' &
     wait # TODO force sequential for now
     [ "$dir" != performance ] || wait
   done < <({ find $dir -iname 'test_*.sh'; find $dir -iname 'test_*.'"$SHELL"; } | sort -u)
