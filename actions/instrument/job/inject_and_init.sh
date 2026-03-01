2dc6de970d295dbe361f9eac0d967d4774b18b9e
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-01 10:06:08 +0100

    Add conditional logic for FAST_DEB_INSTALL_PRESERVE_ACL

--- actions/instrument/job/inject_and_init.sh
+++ actions/instrument/job/inject_and_init.sh
@@ -94,11 +94,15 @@ if ! type otel.sh && [ -r /var/cache/apt/archives/opentelemetry-shell_*_all.deb 
     control_dir="$(mktemp -d)"
     dpkg-deb --control /var/cache/apt/archives/opentelemetry-shell_*_all.deb "$control_dir"
     if cat "$control_dir"/control | grep -E '^Pre-Depends:|^Depends:' | cut -d ':' -f 2- | tr ',' '\n' | grep -v '|' | tr -d ' ' | cut -d '(' -f 1 | sed 's/awk/gawk/g' | xargs -I '{}' [ -r /var/lib/dpkg/info/'{}'.list ]; then
-      extract_dir="$(mktemp -d)"
-      sudo dpkg-deb --extract /var/cache/apt/archives/opentelemetry-shell_*_all.deb "$extract_dir"
-      tar -C "$extract_dir" -cf - . | sudo tar -C / -xf - --no-overwrite-dir
-      sudo rm -rf "$extract_dir"
-      run eval sudo "$control_dir"/postinst configure '&&' rm -rf "$control_dir"
+      if [ "${FAST_DEB_INSTALL_PRESERVE_ACL:-TRUE}" = TRUE ]; then
+        extract_dir="$(mktemp -d)"
+        sudo dpkg-deb --extract /var/cache/apt/archives/opentelemetry-shell_*_all.deb "$extract_dir"
+        tar -C "$extract_dir" -cf - . | sudo tar -C / -xf - --no-overwrite-dir
+        sudo rm -rf "$extract_dir"
+        run eval sudo "$control_dir"/postinst configure '&&' rm -rf "$control_dir"
+      else
+        sudo dpkg-deb --extract /var/cache/apt/archives/opentelemetry-shell_*_all.deb / && run eval sudo "$control_dir"/postinst configure '&&' rm -rf "$control_dir"
+      fi
       export OTEL_SHELL_PACKAGE_VERSION_CACHE_opentelemetry_shell="$(cat ../../../VERSION)"
     else
       rm -rf "$control_dir"
