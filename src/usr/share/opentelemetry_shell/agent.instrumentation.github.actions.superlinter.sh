7254554bd85659faf1eb3d1a97f7f12a61e9cec3
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-01 22:37:10 +0100

    Add GitHub Actions instrumentation for Super Linter

---
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
@@ -0,0 +1,8 @@
+#!/bin/false
+
+if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ "${GITHUB_ACTION_REPOSITORY:-}" = super-linter/super-linter ] && \[ -d /action/lib/ ] && ! \[ -r /action/lib/.otel ]; then
+  \touch /action/lib/.otel
+  \find /action/lib -iname '*.sh' | while \read -r path; do
+    \sed -i -E 's/("\$\{[a-zA-Z0-9_]*\[@\]\}")/\\eval "$(_otel_escape_args \1)"/g' "$path" || \true
+  done
+fi
