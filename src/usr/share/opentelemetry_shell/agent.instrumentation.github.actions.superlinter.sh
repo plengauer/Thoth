9fe8793e9964b4b0c868de7a6e64e667c88c847d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-02 12:03:02 +0100

    Fix syntax in superlinter instrumentation script

--- src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
@@ -3,6 +3,6 @@
 if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ "${GITHUB_ACTION_REPOSITORY:-}" = super-linter/super-linter ] && \[ -d /action/lib/ ] && ! \[ -r /action/lib/.otel ]; then
   \touch /action/lib/.otel
   \find /action/lib -iname '*.sh' | while \read -r path; do
-    \sed -i -E 's/| ("\$\{[a-zA-Z0-9_]*\[@\]\}")/| \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
+    \sed -i -E 's/\| ("\$\{[a-zA-Z0-9_]*\[@\]\}")/| \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
   done
 fi
