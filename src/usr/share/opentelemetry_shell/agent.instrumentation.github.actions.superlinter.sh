bf9538153f05929a73337d2659f62d14c1fcce2f
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-02 21:31:49 +0100

    Update agent.instrumentation.github.actions.superlinter.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
@@ -1,7 +1,7 @@
 #!/bin/false
 
 if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ "${GITHUB_ACTION_REPOSITORY:-}" = super-linter/super-linter ] && \[ -d /action/lib/ ] && ! \[ -r /action/lib/.otel ]; then
-  \parallel --help 1>&2
+  \parallel --help 1>&2 # SKIP_DEPENDENCY_CHECK
   \touch /action/lib/.otel
   \find /action/lib -iname '*.sh' | while \read -r path; do
     \sed -i -E 's/\| ("\$\{[a-zA-Z0-9_]*\[@\]\}")/| \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
