7b562d251b9c8d396cc8f16035f626a1b3a87be1
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-03-16 20:26:57 +0100

    Update install.sh

--- actions/instrument/shared/install.sh
+++ actions/instrument/shared/install.sh
@@ -7,19 +7,20 @@ ensure_installed jq curl wget "$@" || (sudo apt-get update && ensure_installed j
 if ! type otel.sh 2> /dev/null; then
   echo "::debug::Installing ..."
   action_tag_name="${GITHUB_ACTION_REF#*@}"
+  version="$(cat ../../../VERSION)"
   arch="$(arch | sed s/x86_64/amd64/g | sed 's/le$/el/g')"
-  if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && [ -f "$GITHUB_WORKSPACE"/opentelemetry-shell_*_"$arch".deb ]; then
+  if [ "$GITHUB_REPOSITORY" = "$GITHUB_ACTION_REPOSITORY" ] && [ -f "$GITHUB_WORKSPACE"/opentelemetry-shell_"$version"_"$arch".deb ]; then
     echo "::debug::Installing local debian ..."
-    sudo -E -H apt-get install -y "$GITHUB_WORKSPACE"/opentelemetry-shell_*_"$arch".deb
+    sudo -E -H apt-get install -y "$GITHUB_WORKSPACE"/opentelemetry-shell_"$version"_"$arch".deb
   else
     echo "::debug::Downloading debian and installing ..."
-    debian_file=/var/cache/apt/archives/opentelemetry-shell_"$(cat ../../../VERSION)"_"$arch".deb
-    [ "$action_tag_name" = main ] || action_tag_name=v"$(cat ../../../VERSION)"
+    debian_file=/var/cache/apt/archives/opentelemetry-shell_"$version"_"$arch".deb
+    [ "$action_tag_name" = main ] || action_tag_name=v"$version"
     GITHUB_REPOSITORY="$GITHUB_ACTION_REPOSITORY" gh_release "$action_tag_name" | jq '.assets[] | select(.name | endswith(".deb")) | .url' -r | xargs -0 -I '{}' wget --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" --header 'Accept: application/octet-stream' -O - '{}' | sudo tee "$debian_file" > /dev/null
     sudo -E -H apt-get -o Binary::apt::APT::Keep-Downloaded-Packages=true install -y "$debian_file"
   fi || (
       echo ::warning::'Cannot find release for specified tag, falling back to latest. This may be due to tags that haven'\''t finished building yet (like in a fresh fork), in which case this will resolve automatically.' >&2
-      curl --fail -L -s -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" "${GITHUB_API_URL:-https://api.github.com}"/repos/"$GITHUB_ACTION_REPOSITORY"/releases/latest | jq '.assets[] | select(.name | endswith("_all.deb") or .name | endswith(".deb")) | .url' -r | head -n 1 | xargs -0 -I '{}' wget --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" --header 'Accept: application/octet-stream' -O /tmp/package.deb '{}'
+      curl --fail -L -s -H "Authorization: Bearer $INPUT_GITHUB_TOKEN" "${GITHUB_API_URL:-https://api.github.com}"/repos/"$GITHUB_ACTION_REPOSITORY"/releases/latest | jq '.assets[] | select(.name | endswith("_all.deb")) | .url' -r | head -n 1 | xargs -0 -I '{}' wget --header "Authorization: Bearer $INPUT_GITHUB_TOKEN" --header 'Accept: application/octet-stream' -O /tmp/package.deb '{}'
       sudo -E -H apt-get install -y /tmp/package.deb
       rm /tmp/package.deb
     ) || ( echo ::warning::'Cannot find any release in repository, falling back to root repository' >&2 && wget -O - https://raw.githubusercontent.com/plengauer/Thoth/main/INSTALL.sh | sh -e )
