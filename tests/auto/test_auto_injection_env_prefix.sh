2ae31567854d1f0a36bf4e36624e5fe0e43e9dee
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-06-05 12:42:54 +0200

    Update test_auto_injection_env_prefix.sh

--- tests/auto/test_auto_injection_env_prefix.sh
+++ tests/auto/test_auto_injection_env_prefix.sh
@@ -6,9 +6,3 @@ assert_equals 0 $?
 assert_equals "testvalue" "$output"
 span="$(resolve_span '.name == "printenv MY_ENV_VAR"')"
 assert_not_equals "" "$span"
-
-if type timeout 1>/dev/null 2>&1; then
-  output="$(MY_ENV_VAR=testvalue timeout 5 printenv MY_ENV_VAR)"
-  assert_equals 0 $?
-  assert_equals "testvalue" "$output"
-fi
