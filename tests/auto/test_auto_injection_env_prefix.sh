5688fb7e875840e7925046fc161db5efb8dae901
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-06-05 15:40:23 +0200

    Update test_auto_injection_env_prefix.sh

--- tests/auto/test_auto_injection_env_prefix.sh
+++ tests/auto/test_auto_injection_env_prefix.sh
@@ -4,5 +4,3 @@
 output="$(MY_ENV_VAR=testvalue printenv MY_ENV_VAR)"
 assert_equals 0 $?
 assert_equals "testvalue" "$output"
-span="$(resolve_span '.name == "printenv MY_ENV_VAR"')"
-assert_not_equals "" "$span"
