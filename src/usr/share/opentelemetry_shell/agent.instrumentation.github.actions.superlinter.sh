d6a7e1e216bb53388fac39a49eebb9803109c252
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-06-07 22:59:00 +0200

    Update agent.instrumentation.github.actions.superlinter.sh

--- src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
+++ src/usr/share/opentelemetry_shell/agent.instrumentation.github.actions.superlinter.sh
@@ -6,9 +6,8 @@ if \[ "${GITHUB_ACTIONS:-false}" = true ] && \[ "${GITHUB_ACTION_REPOSITORY:-}" 
   \export OTEL_SHELL_CONFIG_OBSERVE_SIGNALS=FALSE
   \export OTEL_SHELL_CONFIG_OBSERVE_PIPES=FALSE
   \find /action/lib -iname '*.sh' | while \read -r path; do
-    :
-    # \sed -i -E 's/\| ("\$\{[a-zA-Z0-9_]*\[@\]\}")/| \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
-    # \sed -i -E 's/! ("\$\{[a-zA-Z0-9_]*\[@\]\}")/! \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
-    # \sed -i -E 's/^PARALLEL_COMMAND_OUTPUT=/if \command -v _otel_inject_parallel_arguments >\/dev\/null 2>\&1; then _otel_inject_parallel_arguments "${PARALLEL_COMMAND[@]}" >\&2; fi; PARALLEL_COMMAND_OUTPUT=/g' "$path" || \true
+    \sed -i -E 's/\| ("\$\{[a-zA-Z0-9_]*\[@\]\}")/| \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
+    \sed -i -E 's/! ("\$\{[a-zA-Z0-9_]*\[@\]\}")/! \\eval "$(_otel_escape_args \1)"/g' "$path" || \true
+    \sed -i -E 's/^PARALLEL_COMMAND_OUTPUT=/if \command -v _otel_inject_parallel_arguments >\/dev\/null 2>\&1; then _otel_inject_parallel_arguments "${PARALLEL_COMMAND[@]}" >\&2; fi; PARALLEL_COMMAND_OUTPUT=/g' "$path" || \true
   done
 fi
