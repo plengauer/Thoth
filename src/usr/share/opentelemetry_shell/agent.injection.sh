c6fbf13959fa767eb97e80249f2865ceca941a7d
Philipp Lengauer <100447901+plengauer@users.noreply.github.com>
2026-08-10 12:00:50 +0200

    Refactor agent.injection.sh script
    Refactor agent injection script for improved functionality and clarity.

--- src/usr/share/opentelemetry_shell/agent.injection.sh
+++ src/usr/share/opentelemetry_shell/agent.injection.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-if [ $# -eq 0 ]; then
-  echo "Usage: otel4sh <command> [args...]" >&2
-  exit 1
-fi
-OTEL_SHELL_AUTO_INJECTED=TRUE . /usr/bin/otel.sh
-exec env "$@"
+export OTEL_SHELL_AUTO_INJECTED=TRUE
+. otel.sh
+unset OTEL_SHELL_AUTO_INJECTED
+\unalias env
+_otel_inject_env env "$@"
